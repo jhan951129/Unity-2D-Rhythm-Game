2b87b0b326c18646d329508998322ac33cf57e12
jhan951129 <54831633+jhan951129@users.noreply.github.com>
2019-09-18 14:57:19 +0900

    키보드 이벤트 애니메이션 & 판정 점수, 콤보 기록 처리

--- Unity 2D Rhythm Game/Assets/Scripts/NoteBehavior.cs
+++ Unity 2D Rhythm Game/Assets/Scripts/NoteBehavior.cs
@@ -31,7 +31,7 @@ public class NoteBehavior : MonoBehaviour
         if(Input.GetKey(keyCode))
         {
             //해당 노트에 대한 판정을 진행합니다.
-            Debug.Log(judge);
+            GameManager.instance.processJudge(judge, noteType);
             //노트가 판정선에 닿기 시작한 이후로는 해당 노트를 제거합니다.
             if (judge != GameManager.judges.NONE) gameObject.SetActive(false);
         }
@@ -55,6 +55,7 @@ public class NoteBehavior : MonoBehaviour
         else if (other.gameObject.tag == "Miss Line")
         {
             judge = GameManager.judges.MISS;
+            GameManager.instance.processJudge(judge, noteType);
             gameObject.SetActive(false);
         }
     }
